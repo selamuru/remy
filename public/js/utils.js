704fa1f3b93f893b212adec053e7da77c44b1599
Jonathan Lew <jdlew@Jonathans-MacBook-Pro.local>
2014-09-30 22:29:55 -0700

    [UI] Add search results area.

---
+++ public/js/utils.js
@@ -0,0 +1,10 @@
+<!--
+  function toggle_visibility(id) {
+    var element = document.getElementById(id);
+    if (element.style.display == 'block') {
+      element.style.display = 'none';
+    } else {
+      element.style.display = 'block';
+    }
+  }
+//-->
