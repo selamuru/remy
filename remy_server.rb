9223874f25fa00c33741b31218cc2038f69edfaf
Sura Elamurugu <sura@ooyala.com>
2014-10-11 23:08:40 -0700

    Add API and associated class to search for a dish name through elasticsearch

--- remy_server.rb
+++ remy_server.rb
@@ -1,4 +1,22 @@
 require "sinatra"
+require "stretcher"
+require "slim"
+
+class Menus
+  @es = Stretcher::Server.new("http://localhost:9200")
+
+  def self.match(name: "elasticsearch", size: 100)
+    @es.index(:menus).search size: size, query: { term: { name: name } }
+  end
+end
+
+before do
+  @json_logs = {}
+end
+
+after do
+  env["json_logs"] = @json_logs
+end
 
 get "/" do
   File.read(File.join('public', 'index.html'))
@@ -6,4 +24,9 @@ end
 
 get "/healthz" do
   "Remy is healthy"
+end
+
+get "/dish/:name" do
+  dishes = Menus.match(name: params[:name])
+  dishes.raw_plain["hits"]["hits"].map { |dish| dish["_source"] }.to_json
 end
