ffc1fdcbf9fbbc85cb309e12527f84b95bf4bf1f
Jonathan Lew <jdlew@Jonathans-MacBook-Pro.local>
2014-10-12 22:24:06 -0700

    Display unlimited results to page; Clear previous results.

--- public/js/remy.js
+++ public/js/remy.js
@@ -10,38 +10,56 @@
         var results = $.parseJSON(data);
         console.log(results);
         if (results && results.length !== 0) {
-          // TODO(jdlew): This needs to be templatized so values can be
-          // substitued easily.
-          var markup =
-          '<div class="row">' +
-            '<div class="col-md-7">' +
-              '<a href="#">' +
-                '<img class="img-responsive" src="http://placehold.it/700x300" alt="">' +
-              '</a>' +
-            '</div>' +
-            '<div class="col-md-5">' +
-              '<h3>Restaurant One</h3>' +
-              '<h4>Rating - Address - Distance</h4>' +
-              '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.</p>' +
-              '<a class="btn btn-primary" href="#">View Website <span class="glyphicon glyphicon-chevron-right"></span></a>' +
-            '</div>' +
-          '</div>' +
+          // clear previous search results
+          $("#results").empty();
 
-          '<hr>';
-
-          // iterate over first 5 elements in API response
+          // iterate over each element in API response
           $.each(results, function(index, value) {
             console.log(value);
-            console.log($("#results"));
+            // TODO(jdlew): This needs to be templatized so values can be
+            // substitued easily.
+            var markup =
+              '<div class="row">' +
+                '<div class="col-md-7">' +
+                  '<a href="#">' +
+                    '<img class="img-responsive" src="http://placehold.it/700x300" alt="">' +
+                  '</a>' +
+                '</div>' +
+                '<div class="col-md-5">';
+
             $.each(this, function(key, value) {
-              console.log(key);
-              console.log(value);
-              $("#results").append(markup);
+              console.log("Key: " + key + "; Value: " + value);
+              switch (key) {
+                case "restaurant":
+                  markup += '<h4>' + value + '</h4>';
+                  markup += '<h4>Rating - Address - Distance</h4>';
+                  break;
+                case "dish_type":
+                  markup += '<h5>' + value + '</h5>';
+                  break;
+                case "name":
+                  markup += '<h5>' + value + '</h5>';
+                  break;
+                case "description":
+                  markup += '<p>' + value + '<p>';
+                  break;
+                default:
+                  console.log("Unknown key: ", key);
+              }
             });
+            markup +=
+                '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. ' +
+                'Laudantium veniam exercitationem expedita laborum at voluptate. ' +
+                'Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.</p>' +
+                '<a class="btn btn-primary" href="#">View Website <span class="glyphicon glyphicon-chevron-right"></span></a>' +
+              '</div>' +
+            '</div>' +
+            '<hr>';
+            $("#results").append(markup);
           });
 
           // display results
-          toggle_visibility('results', true);
+          toggle_visibility("results", true);
         } else {
           console.log("Empty response", results);
         }
